c573b3fbd15f87177408435a5b34ab8e0a1f3959
t-rodrjua <juan.pablo.rodriguez@autodesk.com>
2022-08-26 09:35:59 -0500

    added the general structure of the app

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <footer>code by JP</footer>;
+};
+
+export default Footer;
---
+++ src/components/main/Main.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = () => {
+  return <main>this is the main section</main>;
+};
+
+export default Main;
