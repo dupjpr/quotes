ab5bec1bc60e321a54103a98ceba7bdc8c907246
t-rodrjua <juan.pablo.rodriguez@autodesk.com>
2022-08-30 08:44:57 -0500

    refactored the stage manager

--- src/components/main/QuotesList/listGenerator.tsx
+++ src/components/main/QuotesList/listGenerator.tsx
@@ -1,9 +1,9 @@
 export const listGenerator = (data: Array<any>) => {
-  let newList: Array<{}> = [];
+  let newList: any = "";
 
   for (let i = 0; i < 10; i++) {
     const id = Math.floor(Math.random() * 1000);
     newList = [...newList, data[id]];
   }
-  return newList;
+  return newList && newList;
 };
